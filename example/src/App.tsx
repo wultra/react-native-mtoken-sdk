b84dc556a8176c3570e171d22bb6672281bbf1db
Jan Kobersky <kober32@gmail.com>
2024-08-09 17:23:18 +0200

    Experimental operaiton list

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,79 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply, testPowerAuthCode } from 'react-native-mtoken-sdk';
-import { type PowerAuthActivationCode } from 'react-native-powerauth-mobile-sdk';
+import { StyleSheet, View, Text, Alert, Button } from 'react-native';
+import { MToken } from 'react-native-mtoken-sdk';
+import { PowerAuth, PowerAuthActivation, PowerAuthAuthentication, type PowerAuthActivationCode, type PowerAuthActivationStatus } from 'react-native-powerauth-mobile-sdk';
 
 export default function App() {
 
-  // to test the module
-  const [result, setResult] = useState<number | undefined>();
-  const [actCode, setActCode] = useState<PowerAuthActivationCode | undefined>();
+  const powerAuth = new PowerAuth("test-instance");
+  const mtoken = new MToken(powerAuth);
+  const pin = "1111";
+
+  const [isActivated, setIsActivated] = useState<boolean | undefined>();
+  const [status, setStatus] = useState<PowerAuthActivationStatus | undefined>();
 
   useEffect(() => {
-    multiply(3, 7).then(setResult);
-    testPowerAuthCode().then(setActCode)
+    prepare().catch(log);
   }, []);
+
+  const log = (err: any) => {
+    console.log(err);
+    Alert.alert("Error", `${JSON.stringify(err)}`);
+  }
+
+  const prepare = async (): Promise<void> => {
+    if (!await powerAuth.isConfigured()) {
+      await powerAuth.configure(
+        "gfv6saZiwmtOAMk9iD1xyg==",
+        "Pgd67vpBT6/Y+2fNBt7Sxg==",
+        "BFNObd28hFHYfdAgYgb6oK+LFlO69WEwLXaU4dxMoQFC+/dZOusMvkmTNahC8Os3aDhzRZP8+J3gw6irSEOROY4=",
+        "https://powerauth-dev.westeurope.cloudapp.azure.com/enrollment-server/",
+        false
+      )
+    }
+
+    let isActivated = await powerAuth.hasValidActivation();
+    setIsActivated(isActivated);
+    if (isActivated) {
+      await updateStatus()
+    }
+  }
+
+  const updateStatus = async () => {
+    setStatus(undefined);
+    setStatus(await powerAuth.fetchActivationStatus());
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <Text>Parsed activaiton code: {actCode?.activationCode}</Text>
+      <Text>Is Activated: {isActivated ? "yes" : "no"}</Text>
+      <Text>Activation status: {JSON.stringify(status)}</Text>
+      <Button title='Activate and commit' onPress={async () => {
+        try {
+          await powerAuth.createActivation(PowerAuthActivation.createWithActivationCode("XGDD6-RC2NU-XVU4D-43N3Q", "React-Native-MtokenSDK-example"));
+          await powerAuth.commitActivation(PowerAuthAuthentication.commitWithPassword(pin));
+          Alert.alert("OK");
+        } catch (err) {
+          log(err);
+        }
+      }} />
+      <Button title='Update status' onPress={async () => {
+        try {
+          await updateStatus();
+        } catch (err) {
+          log(err);
+        }
+      }} />
+      <Button title='Fetch operations' onPress={async () => {
+        try {
+          let result = await mtoken.operationList();
+          let jsonResult = JSON.stringify(result);
+          console.log(jsonResult);
+          Alert.alert(jsonResult);
+        } catch (err) {
+          log(err);
+        }
+      }} />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,58 @@
-import { PowerAuthActivationCodeUtil, type PowerAuthActivationCode } from 'react-native-powerauth-mobile-sdk';
+import { PowerAuth, PowerAuthAuthentication } from 'react-native-powerauth-mobile-sdk';
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+export class MToken {
+
+  acceptLanguage = "en";
+
+  private pa: PowerAuth;
+  private baseURL: String;
+
+  constructor(powerAuth: PowerAuth) {
+    this.pa = powerAuth;
+    this.baseURL = powerAuth.configuration?.baseEndpointUrl ?? ""
+    if (!this.baseURL.endsWith("/")) {
+      this.baseURL = this.baseURL + "/";
+    }
+  }
+
+  async operationList(): Promise<any> {
+
+    return await (await this.postSignedWithToken(
+      {},
+      PowerAuthAuthentication.possession(),
+      "api/auth/token/app/operation/list",
+      "possession_universal"
+    )).json();
+  }
+
+  private async postSignedWithToken(
+    request: any,
+    auth: PowerAuthAuthentication,
+    endpoindPath: string,
+    tokenName: string
+  ): Promise<Response> {
+
+    let method = "POST";
+    let url = this.baseURL + endpoindPath;
+    let token = await this.pa.tokenStore.requestAccessToken(tokenName, auth);
+    let paHeader = await this.pa.tokenStore.generateHeaderForToken(token.tokenName);
+
+    let jsonType = "application/json";
+    let headers = new Headers();
+    headers.set("Accept", jsonType);
+    headers.set("Content-Type", jsonType);
+    headers.set("Accept-Language", this.acceptLanguage);
+    headers.set("User-Agent", "react-native-mtoken-sdk");
+    headers.set(paHeader.key, paHeader.value);
+
+    let result = await fetch( url, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(request)
+      }
+    )
+
+    return result
+  }
 }
 
-export function testPowerAuthCode(): Promise<PowerAuthActivationCode > {
-  return PowerAuthActivationCodeUtil.parseActivationCode("AAAAA-AAAAA-AAAAA-AAAAA");
-}
